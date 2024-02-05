8b45942c53aee81fd2223956a754d03d0b13ef3a
Sandeep Sundar Lenka <82632232+big-peanut@users.noreply.github.com>
2024-02-05 15:30:35 +0530

    Update auth.controller.ts

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,10 +3,12 @@ import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
+// Controller handling authentication-related routes
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('/signup')
+  // Endpoint for user registration (sign-up)
   signUp(
     @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
   ): Promise<void> {
@@ -14,6 +16,7 @@ export class AuthController {
   }
 
   @Post('/signin')
+  // Endpoint for user login (sign-in) and JWT generation
   signIn(
     @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
   ): Promise<{ accessToken: string }> {
