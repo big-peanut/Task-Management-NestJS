4915e8c1d7f8d8a3f5c76fe56341e350dffa3af4
Sandeep Sundar Lenka <82632232+big-peanut@users.noreply.github.com>
2024-02-05 15:25:48 +0530

    Update create-task.dto.ts

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,9 +1,10 @@
 import { IsNotEmpty } from 'class-validator';
 
+// Data transfer object (DTO) for creating a new task
 export class CreateTaskDto {
   @IsNotEmpty()
-  title: string;
+  title: string; // Title of the task, should not be empty
 
   @IsNotEmpty()
-  description: string;
+  description: string; // Description of the task, should not be empty
 }
