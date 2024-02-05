b6877f99e45e0b407c1c610c196534bb6d99432f
Sandeep Sundar Lenka <82632232+big-peanut@users.noreply.github.com>
2024-02-05 15:29:07 +0530

    Update auth.service.ts

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -19,43 +19,59 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
+  // Sign up a new user with the provided credentials
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
 
+    // Generate a salt for password hashing
     const salt = await bcrypt.genSalt();
 
+    // Create a new User instance
     const user = new User();
     user.username = username;
     user.salt = salt;
 
+    // Hash the provided password and set it for the user
     user.password = await this.hashPassword(password, salt);
 
     try {
+      // Save the user to the database
       await user.save();
     } catch (error) {
+      // Handle unique constraint violation (duplicate username)
       if (error.code === '23505') {
         throw new ConflictException('Username already exists');
       } else {
+        // Handle other database errors
         throw new InternalServerErrorException();
       }
     }
   }
 
+  // Sign in a user with the provided credentials and generate an access token
   async signIn(
     authCredentialsDto: AuthCredentialsDto,
   ): Promise<{ accessToken: string }> {
     const { username, password } = authCredentialsDto;
+
+    // Find the user based on the provided username
     const user = await this.userRepository.findOne({ where: { username } });
+
+    // Validate the user's password
     if (user && user.validatePassword(password)) {
+      // Create a JWT payload with the username
       const payload: JwtPayload = { username };
+
+      // Sign the JWT payload to generate an access token
       const accessToken = await this.jwtService.sign(payload);
+
+      // Return the access token
       return { accessToken };
     } else {
+      // Return null if the user is not found or password is incorrect
       return null;
     }
   }
 
-  private async hashPassword(password: string, salt: string): Promise<string> {
-    return bcrypt.hash(password, salt);
-  }
-}
+  // Hash the provided password using the given salt
+  private
