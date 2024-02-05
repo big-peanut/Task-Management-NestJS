dc554f7861340ff0f266a943317e95d7dd5e3084
Sandeep Sundar Lenka <82632232+big-peanut@users.noreply.github.com>
2024-02-05 15:29:56 +0530

    Update auth.module.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -8,20 +8,24 @@ import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './jwt-strategy';
 
 @Module({
+  // Import necessary modules for authentication
   imports: [
     PassportModule.register({
-      defaultStrategy: 'jwt',
+      defaultStrategy: 'jwt', // Use the JWT strategy by default
     }),
     JwtModule.register({
-      secret: 'topSecret51',
+      secret: 'topSecret51', // Secret key used for signing JWTs
       signOptions: {
-        expiresIn: 3600,
+        expiresIn: 3600, // Token expiration time (1 hour)
       },
     }),
-    TypeOrmModule.forFeature([User]),
+    TypeOrmModule.forFeature([User]), // Include the User entity for TypeORM
   ],
+  // Specify the controllers provided by this module
   controllers: [AuthController],
+  // Specify the services and strategies provided by this module
   providers: [JwtStrategy, AuthService],
+  // Export the JwtStrategy and PassportModule for use in other modules
   exports: [JwtStrategy, PassportModule],
 })
 export class AuthModule {}
