b0b785bb138bb9a14974554d9067515ae6d11a9f
Sandeep Sundar Lenka <82632232+big-peanut@users.noreply.github.com>
2024-02-05 15:20:36 +0530

    Update tasks.module.ts

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -6,8 +6,16 @@ import { Task } from './task.entity';
 import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Task]), AuthModule],
+  // Import TypeOrmModule to make the Task entity available to this module
+  imports: [TypeOrmModule.forFeature([Task]), 
+
+    // Import AuthModule to include authentication-related functionality
+    AuthModule],
+
+  // Specify the controller responsible for handling HTTP requests
   controllers: [TasksController],
+
+  // Specify the service responsible for handling business logic related to tasks
   providers: [TasksService],
 })
 export class TasksModule {}
