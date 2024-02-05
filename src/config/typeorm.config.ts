692358f064cc09a5ddcdd1d0dcf7ea366b1ffdaf
Sandeep Sundar Lenka <82632232+big-peanut@users.noreply.github.com>
2024-02-05 15:26:39 +0530

    Update typeorm.config.ts

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,12 +1,13 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
+// Configuration options for TypeORM module
 export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: '12345678',
-  database: 'taskmanagement',
-  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-  synchronize: true,
+  type: 'postgres', // Database type (PostgreSQL in this case)
+  host: 'localhost', // Database host
+  port: 5432, // Database port
+  username: 'postgres', // Database username
+  password: '12345678', // Database password
+  database: 'taskmanagement', // Database name
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'], // Entities to be included in the database schema
+  synchronize: true, // Auto-create database schema based on entities (for development purposes)
 };
