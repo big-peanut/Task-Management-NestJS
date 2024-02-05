dd519f30d4309232023be9148fdfb14c3f8a5bd5
Sandeep Sundar Lenka <82632232+big-peanut@users.noreply.github.com>
2024-02-05 15:28:13 +0530

    Update jwt-strategy.ts

--- src/auth/jwt-strategy.ts
+++ src/auth/jwt-strategy.ts
@@ -12,19 +12,26 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     @InjectRepository(User)
     private userRepository: Repository<User>,
   ) {
+    // Initialize the JwtStrategy with configuration options
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: 'topSecret51',
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), // Extract JWT from the Authorization header
+      secretOrKey: 'topSecret51', // Secret key used to verify the JWT signature
     });
   }
 
+  // Validate method to check the validity of the JWT payload
   async validate(payload: JwtPayload): Promise<User> {
     const { username } = payload;
+
+    // Find the user based on the username in the JWT payload
     const user = await this.userRepository.findOne({ where: { username } });
 
+    // Throw UnauthorizedException if the user is not found
     if (!user) {
-      throw new UnauthorizedException();
+      throw new UnauthorizedException('Invalid user in JWT payload');
     }
+
+    // Return the validated user
     return user;
   }
 }
