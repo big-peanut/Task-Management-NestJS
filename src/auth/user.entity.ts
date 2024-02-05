8a0dfd18dd8cdb4981b2cfb0cea89058654fd592
Sandeep Sundar Lenka <82632232+big-peanut@users.noreply.github.com>
2024-02-05 15:27:28 +0530

    Update user.entity.ts

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -10,25 +10,29 @@ import * as bcrypt from 'bcrypt';
 import { Task } from 'src/tasks/task.entity';
 
 @Entity()
-@Unique(['username'])
+@Unique(['username']) // Ensure that the username is unique across users
+// User entity representing a registered user in the system
 export class User extends BaseEntity {
   @PrimaryGeneratedColumn()
-  id: number;
+  id: number; // Primary key for the user entity
 
   @Column()
-  username: string;
+  username: string; // Username of the user, should be unique
 
   @Column()
-  password: string;
+  password: string; // Hashed password of the user
 
   @Column()
-  salt: string;
+  salt: string; // Salt used for hashing the password
 
   @OneToMany((type) => Task, (task) => task.user, { eager: true })
-  tasks: Task[];
+  tasks: Task[]; // One-to-Many relationship with tasks, representing tasks associated with the user
 
+  // Method to validate the user's password
   async validatePassword(password: string): Promise<boolean> {
+    // Hash the provided password using the user's salt
     const hash = await bcrypt.hash(password, this.salt);
+    // Compare the hashed password with the stored password
     return hash === this.password;
   }
 }
