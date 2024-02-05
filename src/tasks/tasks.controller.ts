c569b900fa24b1fe1b0593a34912714383dfde60
Sandeep Sundar Lenka <82632232+big-peanut@users.noreply.github.com>
2024-02-05 15:22:57 +0530

    Update tasks.controller.ts

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -20,16 +20,18 @@ import { TaskStatus } from './task-status.enum';
 import { AuthGuard } from '@nestjs/passport';
 
 @Controller('tasks')
-@UseGuards(AuthGuard())
+@UseGuards(AuthGuard()) // Use the Passport authentication guard for this controller
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get('/all')
+  // Get all tasks for the authenticated user
   getAllTasks(@Req() req): Promise<Task[]> {
     return this.tasksService.getAllTasks(req.user);
   }
 
   @Get('/:id')
+  // Get a task by its ID for the authenticated user
   getTaskById(
     @Param('id', ParseIntPipe) id: number,
     @Req() req,
@@ -38,15 +40,19 @@ export class TasksController {
   }
 
   @Post()
+  // Create a new task for the authenticated user
   createTask(@Body() createTaskDto: CreateTaskDto, @Req() req): Promise<Task> {
     return this.tasksService.createTask(createTaskDto, req.user);
   }
+
   @Delete('/:id')
+  // Delete a task by its ID for the authenticated user
   deleteTask(@Param('id', ParseIntPipe) id: number, @Req() req): Promise<void> {
     return this.tasksService.deleteTask(id, req.user);
   }
 
   @Patch('/:id/status')
+  // Update the status of a task by its ID for the authenticated user
   updateTaskStatus(
     @Param('id', ParseIntPipe) id: number,
     @Body('status', TaskStatusValidationPipe) status: TaskStatus,
